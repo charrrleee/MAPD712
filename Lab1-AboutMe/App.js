490e2661e6dd2248cbe804da5ad4c63b96d09f32
charrrleee <charlcpy@gmail.com>
2022-09-08 21:38:22 -0400

    add lab1

---
+++ Lab1-AboutMe/App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.textStyle}>Cheung Pui Yan(Charlene)</Text>
+      <Text style={styles.textStyle}>301252393</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textStyle: {
+    fontSize: 30
+  }
+});
